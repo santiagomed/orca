9813db16c0e871be0a54ebe48065f05f2e022bd8
Santiago Medina <santiagm08@gmail.com>
2023-11-01 14:24:00 -0700

    mistral 7b instruct example

---
+++ examples/quantized/src/main.rs
@@ -0,0 +1,21 @@
+use orca::chains::chain::LLMChain;
+use orca::chains::Chain;
+use orca::llm::quantized::{Model, Quantized};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let model = Quantized::new()
+        .with_model(Model::Mistral7bInstruct)
+        .with_sample_len(99)
+        .load_model_from_path("../../models/mistral-7b-instruct-v0.1.Q4_K_S.gguf")?
+        .build_model()?;
+
+    let pipe = LLMChain::new(&model).with_template("greet", "Hi how are you doing?");
+    let result = pipe.execute("greet").await?;
+
+    println!("{}", result.content());
+
+    Ok(())
+}
